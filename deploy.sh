7747c86a9012ab3bfbfd530fa2e24397f785e566
vijaymanjunathas5 <vijaymanjunathas5@gmail.com>
2022-08-13 07:46:00 +0530

    updated from master

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ Added another line to this file in master
 added another line to test git commit -a m
 adding update line in master
 added line using development branch
+added anothe line to check git merge from master
