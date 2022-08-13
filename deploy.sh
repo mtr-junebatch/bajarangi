8abca5548f13cc0f62bff78e2bd3c3f1d2b6ffa2
vijaymanjunathas5 <vijaymanjunathas5@gmail.com>
2022-08-13 07:50:12 +0530

    updated from dev

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 This file is creaed in MTR master 
 added liine in devbranch
 Added another line to this file in master
+added to test merge from master
 added another line to test git commit -a m
 adding update line in master
 added line using development branch
