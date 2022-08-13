6d5c27d789f831af90451917c97588e8e9e9d341
vijaymanjunathas5 <vijaymanjunathas5@gmail.com>
2022-08-13 07:57:37 +0530

    updated i nmerge

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,4 @@ added another line to test git commit -a m
 adding update line in master
 added line using development branch
 added anothe line to check git merge from master
+updte merger test dev
